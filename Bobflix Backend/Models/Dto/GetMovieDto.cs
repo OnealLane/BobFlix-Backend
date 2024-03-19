711880e31d6ad3ec832cc9ea8545c632b17d35fd
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-03-19 09:18:25 +0100

    Fixed movies endpoint

--- Bobflix Backend/Models/Dto/GetMovieDto.cs
+++ Bobflix Backend/Models/Dto/GetMovieDto.cs
@@ -1,15 +1,17 @@
-﻿namespace Bobflix_Backend.Models.Dto
+﻿using System.Diagnostics.Contracts;
+
+namespace Bobflix_Backend.Models.Dto
 {
     public class GetMovieDto
     {
         public string ImdbId { get; set; }
         public string Title { get; set; }
-        public string Plot { get; set; }
-        public string Poster_url { get; set; }
         public string Director { get; set; }
         public string Released { get; set; }
+        public string PosterUrl { get; set; }
+        public string Plot { get; set; }
         public int CurrentUserRating { get; set; }
-        public int AverageRating { get; set; }
+        public double AvgRating { get; set; }
 
     }
 }
