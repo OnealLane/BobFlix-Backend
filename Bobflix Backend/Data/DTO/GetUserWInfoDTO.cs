5b36408107d466a4dc8b3ca4f0f1fb3be2395fde
Oneallane <oneallane2808@gmail.com>
2024-03-19 13:24:37 +0100

    fix return value of usercontroller

---
+++ Bobflix Backend/Data/DTO/GetUserWInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿using Bobflix_Backend.Models.Dto;
+
+namespace Bobflix_Backend.Data.DTO
+{
+    public class GetUserWInfoDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int AvgRating { get; set; } = 0;
+        public List<GetMovieDto> favouriteMovies { get; set; } = [];
+    }
+}
